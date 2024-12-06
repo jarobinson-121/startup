5b2c0e65bc2e33ec0c914fa361965c4557e440d4
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2024-12-05 23:01:12 -0700

    Update: Made changes to inconsistent naming for login and added another third party api call for joke.

--- service/index.js
+++ service/index.js
@@ -25,12 +25,12 @@ app.use(`/api`, apiRouter);
 
 // CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
-  const user = users[req.body.email];
+  const user = users[req.body.userName];
   if (user) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
-    const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-    users[user.email] = user;
+    const user = { userName: req.body.userName, password: req.body.password, token: uuid.v4() };
+    users[user.userName] = user;
 
     res.send({ token: user.token });
   }
