ae671c384fb08c7321e5a04a915f937dc8ecff59
jarobinson-121 <91813271+jarobinson-121@users.noreply.github.com>
2024-12-05 23:02:04 -0700

    Update: corrected some naming mistakes and error handling to get login and logout working.

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -6,21 +6,36 @@ import Button from 'react-bootstrap/Button';
 export function Authenticated(props) {
   const navigate = useNavigate();
 
+  const [setup, setSetup] = React.useState('Loading...');
+  const [punchline, setPunchline] = React.useState('unknown');
+
   function logout() {
     fetch(`/api/auth/logout`, {
       method: 'delete',
     })
       .catch(() => {
+        console.log('Failed to logout');
         // Logout failed. Assuming offline
       })
       .finally(() => {
         localStorage.removeItem('userName');
+        console.log('Logged out');
         props.onLogout();
       });
   }
 
+  React.useEffect(() => {
+    fetch('https://official-joke-api.appspot.com/random_joke')
+      .then((response) => response.json())
+      .then((data) => {
+        setSetup(data.setup);
+        setPunchline(data.punchline);
+      })
+      .catch(() => {});
+      }, []);
+
   return (
-    <div>
+    <div id="authorized-con">
       <span id="top-secret">
             <img src="Top_Secret.png" alt="Top Secret" width="600" height="100" />
         </span>
@@ -32,6 +47,10 @@ export function Authenticated(props) {
         <Button variant='secondary' onClick={() => logout()}>
           Logout
         </Button>
+      </div>
+        <div id="quote">
+          <p>{setup}</p>
+          <p>{punchline}</p>
       </div>
     </div>
   );
--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -17,19 +17,23 @@ export function Unauthenticated(props) {
   }
 
   async function loginOrCreate(endpoint) {
-    const response = await fetch(endpoint, {
-      method: 'post',
-      body: JSON.stringify({ email: userName, password: password }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-    if (response?.status === 200) {
-      localStorage.setItem('userName', userName);
-      props.onLogin(userName);
-    } else {
-      const body = await response.json();
-      setDisplayError(`⚠ Error: ${body.msg}`);
+    try {
+      const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({ userName: userName, password: password }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      if (response?.status === 200) {
+        localStorage.setItem('userName', userName);
+        props.onLogin(userName);
+      } else {
+        const body = await response.json();
+        setDisplayError(`⚠ Error: ${body.msg}`);
+      }
+    } catch (error) {
+      setDisplayError(`⚠ Error: ${error.message}`);
     }
   }
 
